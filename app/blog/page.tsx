087a06efd85732a6e144214115dc040ee3578d6c
Benjamin Lane <ben@benlane.dev>
2025-04-04 20:39:53 -0700

    Minor styling improvements to blog and projects top pages

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -10,7 +10,7 @@ export default function Page() {
   const blogCount = getBlogPosts().length
   return (
     <section>
-      <h1 className="font-semibold text-2xl mb-8 tracking-tighter">All Articles</h1>
+      <h1 className="font-semibold text-4xl mt-6 mb-8 tracking-tighter">All Articles</h1>
       {blogCount > 0 ? <BlogPosts/> : <p className="mb-4">There's nothing here yet.</p>}
     </section>
   )
